4d8d19035768f934008f39b3c13f1ce079cd905f
thrugl <thorarinn.bjorn@gmail.com>
2022-05-09 16:50:02 +0000

    0.0.19

--- apps/bruce/prebuild.ts
+++ apps/bruce/prebuild.ts
@@ -3,15 +3,16 @@ import { resetDir } from "ntl"
 import { renderRoot, BASE_PATH } from "./banners"
 
 const copy = (file: string) => {
+	const oldFile = `./${file}`
 	const newFile = `${BASE_PATH}/${file}`
-	console.log(`Copying ${file} to ${newFile}`)
-	return copyFile(file, newFile)
+	console.log(`Copying ${oldFile} to ${newFile}`)
+	return copyFile(oldFile, newFile)
 }
 
 try {
 	resetDir(BASE_PATH).then(async () => {
-		await copy('./favicon.ico')
-		await copy('./robots.txt')
+		await copy('favicon.ico')
+		await copy('robots.txt')
 
 		console.log('Rendering banner tree...')
 		return renderRoot()

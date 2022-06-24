d6813b246a7cabe3f65600049122b84a1c6cb709
thrugl <thorarinn.bjorn@gmail.com>
2022-06-24 16:23:42 +0000

    0.0.84

--- apps/fitravel/src/views/Dashboard.vue
+++ apps/fitravel/src/views/Dashboard.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
 import { Page } from "vui/@/fitravel"
+import { DataTable } from "vui/@"
 </script>
 
 <template>
-	<Page secure>
-		Dashboard!
+	<Page secure admin>
+		<h2>Markaðstorg</h2>
+		<DataTable 
+			:cols="[
+				{ header: 'Dags.', key: 'date' },
+				{ header: 'Áfangastaður', key: 'destination' },
+				{ header: 'Sæti í boði', key: 'seats' },
+				{ header: 'Ath', key: 'comment' }
+			]"
+			:rows="[
+				{ destination: 'AGP', seats: 20, date: '04-08-22', comment: 'Trallala' },
+				{ destination: 'TFS', seats: 6, date: '04-08-22', comment: 'Rýmingarsala!' },
+				{ destination: 'PMI', seats: 4, date: '04-08-22', comment: 'Millilent kannski' },
+			]"
+		>
+		</DataTable>
 	</Page>
 </template>

cfd21e886f5793f34831e2ed365d0d3717f87421
thrugl <thorarinn.bjorn@gmail.com>
2022-07-01 13:55:19 +0000

    0.0.105

--- packages/heimdall/fetchEndpoint.ts
+++ packages/heimdall/fetchEndpoint.ts
@@ -2,16 +2,16 @@ import { unrefProps, type R } from "geri"
 import { stringify } from "qs"
 
 //@ts-ignore
-const api = import.meta.env.DEV ? 'http://localhost:9999/.netlify/functions/users' : 'https://spiffy-arithmetic-4222e4.netlify.app/api/users'
+const api = import.meta.env.DEV ? 'http://localhost:9999/.netlify/functions/users' : 'https://fitravel.is/api/users'
 
 export async function fetchEndpoint (method: string, query: R, payload: any, token = '') {
 	const search  = stringify(unrefProps(query))
-	const options = { method, headers: new Headers() }
+	const options = { method, headers: new Headers() } as R
 
 	if (token) options.headers.append('Authorization', `Bearer ${token}`)
 	if (method !== 'GET') options.body = JSON.stringify(unrefProps(payload))
 
-	return fetch(`${api}?${search}`, { method, body, headers }).then(async response => {
+	return fetch(`${api}?${search}`, options).then(async response => {
 		const body = await response.json()
 		if (response.status !== 200) throw body?.error ?? 'Það kom upp villa'
 		return body

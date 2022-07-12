9c7cbfd9dfea2f1e5a37df1a3c54d572a28c4363
thrugl <thorarinn.bjorn@gmail.com>
2022-07-12 16:02:48 +0000

    0.0.120

--- packages/heimdall/fn/createAuthFetch.ts
+++ packages/heimdall/fn/createAuthFetch.ts
@@ -17,7 +17,7 @@ export const createAuthFetch = (_options: AuthFetchOptions) => async (query: R =
 export const createEndpoints = (endpoint: string, ref: Ref<any>, fallback: any, refresh?: () => Promise<any>) => {
 	const _fetch = (method: string, options: Partial<AuthFetchOptions> = {}) => createAuthFetch({ method, endpoint, ...options })
 	const fetch  = _fetch('GET', { endpoint, ref, fallback })
-	const update = (query: R, data: R) => _fetch('PATCH', { endpoint, query, body: data })().then(() => (refresh ?? fetch)(query, data))
+	const update = (query: R, data: R) => _fetch('PATCH', { endpoint, query, data })().then(() => (refresh ?? fetch)(query, data))
 	const create = (data: R) => _fetch('PUT', { endpoint, data })().then(() => (refresh ?? fetch)({}, data))
 
 	return { fetch, update, create }
